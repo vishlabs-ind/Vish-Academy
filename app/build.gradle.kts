3498b257f35d9299a1ba5241fedbfa7ab473b54d
Vishlabs <vishlabs16@gmail.com>
2026-02-19 21:01:16 +0530

    Jubin Code ready

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.gms.google.services)
-
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
 }
