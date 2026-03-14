58a436194d5f17b4add6f463a357c91ecda9d84f
Vishlabs <vishlabs16@gmail.com>
2026-03-14 10:13:00 +0530

    Version 19.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rach.co"
         minSdk = 25
         targetSdk = 36
-        versionCode = 18
-        versionName = "18.0"
+        versionCode = 19
+        versionName = "18.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
