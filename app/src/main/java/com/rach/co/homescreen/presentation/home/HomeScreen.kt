226c5a16c96a56dc317b5414c604d99f01361abb
Surender Kumar <surenderkumar0011996@gmail.com>
2026-03-31 13:28:06 +0530

    add comment

--- app/src/main/java/com/rach/co/homescreen/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/rach/co/homescreen/presentation/home/HomeScreen.kt
@@ -37,7 +37,7 @@ fun HomeScreen(
                 popUpTo("home") { inclusive = true }
             }
         }) {
-            Text("Logout") //log
+            Text("Logout") //log out button
         }
 
     }
