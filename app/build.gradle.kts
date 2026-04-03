9634c8a4ac325cccda48c442879dd43568843590
Vishal Kumar <statusb130@gmail.com>
2026-04-03 21:25:16 +0530

    0989889

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rach.co"
         minSdk = 25
         targetSdk = 36
-        versionCode = 22
-        versionName = "19.1"
+        versionCode = 23
+        versionName = "20.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
