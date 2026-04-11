c9a3fde0b60f7abe12bd804b0ab67fa5a6b5b947
Vishal Kumar <statusb130@gmail.com>
2026-04-11 22:47:40 +0530

    ediuer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rach.co"
         minSdk = 25
         targetSdk = 36
-        versionCode = 26
-        versionName = "20.3"
+        versionCode = 27
+        versionName = "20.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
